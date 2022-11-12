06a3df3da66fc297cb9be813cb97627e56664c38
Samwel Suleiman <94542984+ABlessingFromTheLord@users.noreply.github.com>
2022-11-12 23:51:34 +0100

    Update Bathroom.java

--- Bathroom.java
+++ Bathroom.java
@@ -1,13 +1,14 @@
+
 public class Bathroom extends Room{
     private boolean shower = false;
 
-    public Bathroom(String the_roomName, boolean the_light, boolean the_Shower) {
-        super(the_roomName, the_light);
-        shower = the_Shower;
+    public Bathroom(String the_roomName) {
+        super(the_roomName);
+        shower = false;
     }
 
     public void switchShower(){
-        shower = true;
+        shower = !shower;
         System.out.println("Switched on shower");
     }
 
