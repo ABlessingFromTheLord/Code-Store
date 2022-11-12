34a82e1bc25661d82e4eac600c7d6184357b05f8
Samwel Suleiman <94542984+ABlessingFromTheLord@users.noreply.github.com>
2022-11-12 23:51:03 +0100

    Update Kitchen.java

--- Kitchen.java
+++ Kitchen.java
@@ -1,14 +1,25 @@
 public class Kitchen extends Room{
     private boolean stove = false;
+    private boolean isKitchen = true;
 
-    public Kitchen(String the_roomName, boolean the_light, boolean the_stove) {
-        super(the_roomName, the_light);
-        stove = the_stove;
+    public Kitchen(String the_roomName) {
+        super(the_roomName);
+        stove = false;
     }
 
     public void switchStove(){
-        stove = true;
-        System.out.println("Swtiched on the stove");
+        stove = !stove;
+
+        if(stove == true){
+            System.out.println("Switched on the stove");
+        }
+        else{
+            System.out.println("Switched off the stove");
+        }
     }
 
+    public boolean isKitchen() {
+        return isKitchen;
+    }
 }
+
